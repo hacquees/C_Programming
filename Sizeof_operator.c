46655b18a1fb19d4cb2f1ea26edad161b2fff4e4
Ankit Kumar Singh <57264717+hacquees@users.noreply.github.com>
2022-05-10 00:07:30 +0530

    Size_of_basic_data_types_using_sizeof operator

---
+++ Sizeof_operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+	printf("The size of char data type = %lu\n",sizeof(char));
+	printf("The size of int data type = %lu\n",sizeof(int));
+	printf("The size of short int data type = %lu\n",sizeof(short int));
+	printf("The size of long int data type = %lu\n",sizeof(long int));
+	printf("The size of float data type = %lu\n",sizeof(float));
+	printf("The size of double data type = %lu\n",sizeof(double));
+	printf("The size of long double data type = %lu\n",sizeof(long double));
+	
+	
+}
